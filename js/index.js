9f4876aa19c71031a10a470951f3bcc42d9e7a75
RomainCucu <romain.cunault@gmail.com>
2016-03-30 11:37:52 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -87,7 +87,8 @@ obj.log_callback = function () {
 				document.getElementById(contenuHTML.id).innerHTML = '<div class="alert alert-success alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>Signed successfully!</strong> you are being redirected !</div>';//pour remettre le bouton originel (car gif qui tourne)
 				document.getElementById('colorLogin').className="form-group has-success"; //mettre case en rouge pwd et pseudo (innutile je pense vu que l'on redirige)
 				var avatar = ""+r.data.avatar;
-				window.location = "/html/accueil.html?avatar="+avatar;
+				var avatar = ""+r.data.gender;
+				window.location = "/html/accueil.html?avatar="+avatar+"&gender="+gender;
 			}else if(r.suc_methode == "SIGNUP"){					
 				document.getElementById(contenuHTML.id).innerHTML = contenuHTML.string;//pour remettre le bouton originel (car gif qui tourne)
 				
