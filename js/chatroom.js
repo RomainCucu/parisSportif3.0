c89525e03ba81a3c514faef95cff51e9890f30ca
RomainCucu <romain.cunault@gmail.com>
2016-04-01 13:53:58 +0200

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -18,8 +18,6 @@ obj.envoyerMessageChatRoomFormId = function(){
 		//obj.replace_content_by_animation_GIF_loader('btnChatAjaxLoaderId');
 		data.action = "SENDMESSCHATROOM"
 		data.message = document.getElementById('btn-input').value;
-		data.avatar = getParameterByName('avatar');
-		data.gender = getParameterByName('gender');
 		obj.post(data, obj.log_callback);
 		event.preventDefault();
 	};
@@ -166,14 +164,3 @@ timeSince = function(date) {
 };
 
 obj.start();
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    url = url.toLowerCase(); // This is just to avoid case sensitiveness  
-    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
