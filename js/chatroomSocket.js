57ab615cd6aa2f751036d307d7fabbd08d8a10d6
RomainCucu <romain.cunault@gmail.com>
2016-06-07 12:50:09 +0200

    Update chatroomSocket.js

--- js/chatroomSocket.js
+++ js/chatroomSocket.js
@@ -51,7 +51,7 @@ function insereMessage(pseudo, gender, avatar, message, d) {
 		+'<div class="header">'
 		+'<strong class="primary-font">'+pseudo+'</strong>'
 		+'<small class="pull-right text-muted">'
-		+'<i class="fa fa-clock-o fa-fw"></i> '+12+' ago'
+		+'<i class="fa fa-clock-o fa-fw"></i> '+timeSince(d)+' ago'
 		+'</small>'
 		+'</div>'
 		+'<p>'
@@ -67,7 +67,7 @@ function insereMessage(pseudo, gender, avatar, message, d) {
 		+'<div class="chat-body clearfix">'
 		+'<div class="header">'
 		+'<small class=" text-muted">'
-		+'<i class="fa fa-clock-o fa-fw"></i>'+12+' ago</small>'
+		+'<i class="fa fa-clock-o fa-fw"></i>'+timeSince(d)+' ago</small>'
 		+'<strong class="pull-right primary-font">'+pseudo+'</strong>'
 		+'</div>'
 		+'<p>'
@@ -79,4 +79,32 @@ function insereMessage(pseudo, gender, avatar, message, d) {
 	//on insere le message dans le chat
 	$('#chatRoomId').prepend(str);
 	compteur++;
+};
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
 };
