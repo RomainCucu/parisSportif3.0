1e2ba7157c892c24175da5a4cffd8c2b43dd5acb
RomainCucu <romain.cunault@gmail.com>
2016-06-08 14:33:37 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -80,7 +80,7 @@ var remplirTableauVoteVainqueurs = function (autresVotesObj){
     		var vote1 = arrPaysEuro[parseInt(autresVotesObj[key].VAINQUEURSEURO2016.VOTER1EURO)];
     		var vote2 = arrPaysEuro[parseInt(autresVotesObj[key].VAINQUEURSEURO2016.VOTER2EURO)];
     		var vote3 = arrPaysEuro[parseInt(autresVotesObj[key].VAINQUEURSEURO2016.VOTER3EURO)];
-    		if(i%2 == 0{
+    		if(i%2 == 0){
     			str += '<tr class="danger"><td>'+pseudo+'</td><td>'+vote1+'</td><td>'+vote2+'</td><td>'+vote3+'</td></tr>'
     		}else{
     			str += '<tr class="info"><td>'+pseudo+'</td><td>'+vote1+'</td><td>'+vote2+'</td><td>'+vote3+'</td></tr>'	
