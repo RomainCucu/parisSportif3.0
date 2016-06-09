e7be792e2dbaf40372dee887c218768445a87faf
RomainCucu <romain.cunault@gmail.com>
2016-06-09 14:21:25 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -12,6 +12,52 @@ var groupeD = ["Espagne", "République tchèque", "Turquie",   "Croatie"];
 var groupeE = ["Belgique", "Italie", "Irlande",   "Suède"];
 var groupeF = ["Portugal", "Islande", "Autriche",   "Hongrie"];
 
+/************************************************************************** */
+//pour afficher les poules
+var remplirPoule = function(){
+	var i = "";
+	var groupeTmp = groupeA;
+	//on affiche les divs
+	for (var j = 0 ; j <6 ; j++){
+		if( j == 0 ) {
+			i = 'A';
+			groupeTmp = groupeA;
+		}
+		if( j == 1 ){
+			i = 'B';
+			groupeTmp = groupeB;
+		}
+		if( j == 2 ){
+			i = 'C';
+			groupeTmp = groupeC;
+		}
+		if( j == 3 ) {
+			i = 'D';
+			groupeTmp = groupeD;
+		}
+		if( j == 4 ) {
+			i = 'E';
+			groupeTmp = groupeE;
+		}
+		if( j == 5 ) {
+			i = 'F';
+			groupeTmp = groupeF;
+		}
+		document.getElementById('remplirPoule').innerHTML += '<div class="col-md-4"><div class="chat-panel panel panel-default"><div class="panel-heading"><i class="fa fa-comments fa-fw"></i>Groupe '+i+'</div><!-- /.panel-heading --><div class="panel-body"><div class="row"><div class="col-lg-12"><form id="groupe_'+i+'_form_id"><div class="form-group"><label for="groupe_'+i+'_select_id_1">Sélectionner le premier du groupe A:</label><p class="text-primary col-md-12" id="groupe_'+i+'_select_voted_1" style="display:none">Vous avez voté :</p><select class="form-control" id="groupe_'+i+'_select_id_1" style=""></select></div><div class="form-group"><label for="groupe_'+i+'_select_id_2">Sélectionner le second du groupe A:</label><p class="text-primary col-md-12" id="groupe_'+i+'_select_voted_2" style="display:none">Vous avez voté :</p><select class="form-control" id="groupe_'+i+'_select_id_2" style=""></select></div><button type="submit" class="btn btn-default" id="groupe_'+i+'_btn_id">Submit</button><img id="groupe_'+i+'_gif_submit" src="../images/ajax-loader-mid.gif" style="height:auto; width:auto; display:none;"><p class="text-success" id="groupe_'+i+'_submit_OK" style="height:auto; width:auto; display:none;"> Vote Envoyé !</p><p class="text-danger" id="groupe_'+i+'_submit_KO" style="height:auto; width:auto; display:none;"> Vote NON Envoyé ! rééssayez plus tard !</p><p class="text-warning" id="groupe_'+i+'_submit_KO_doublon" style="height:auto; width:auto; display:none;"> Vote NON Envoyé ! Choisir une équipe différente pour chaque classement !</p></form></div></div></div></div></div>';
+		//on rempli les select
+		for (var z in groupesEuro){
+			if(z <4){
+				document.getElementById('groupe_'+i+'_select_id_1').innerHTML += "<option value="+z+">"+groupeTmp[z]+"</option>";
+	  			document.getElementById('groupe_'+i+'_select_id_2').innerHTML += "<option value="+z+">"+groupeTmp[z]+"</option>";
+			}
+		}
+	}
+};
+//affiche le html
+remplirPoule();
+/************************************************************************** */
+
+
 
 obj.post = function (data, callback) {	
     var xhr = new XMLHttpRequest();
@@ -201,49 +247,5 @@ var eraseCookie= function(){
 
 //recupere les votes et autres
 obj.post({action:'RECUPERERINFOS'},obj.log_callback);
-/************************************************************************** */
-//pour afficher les poules
-var remplirPoule = function(){
-	var i = "";
-	var groupeTmp = groupeA;
-	//on affiche les divs
-	for (var j = 0 ; j <6 ; j++){
-		if( j == 0 ) {
-			i = 'A';
-			groupeTmp = groupeA;
-		}
-		if( j == 1 ){
-			i = 'B';
-			groupeTmp = groupeB;
-		}
-		if( j == 2 ){
-			i = 'C';
-			groupeTmp = groupeC;
-		}
-		if( j == 3 ) {
-			i = 'D';
-			groupeTmp = groupeD;
-		}
-		if( j == 4 ) {
-			i = 'E';
-			groupeTmp = groupeE;
-		}
-		if( j == 5 ) {
-			i = 'F';
-			groupeTmp = groupeF;
-		}
-		document.getElementById('remplirPoule').innerHTML += '<div class="col-md-4"><div class="chat-panel panel panel-default"><div class="panel-heading"><i class="fa fa-comments fa-fw"></i>Groupe '+i+'</div><!-- /.panel-heading --><div class="panel-body"><div class="row"><div class="col-lg-12"><form id="groupe_'+i+'_form_id"><div class="form-group"><label for="groupe_'+i+'_select_id_1">Sélectionner le premier du groupe A:</label><p class="text-primary col-md-12" id="groupe_'+i+'_select_voted_1" style="display:none">Vous avez voté :</p><select class="form-control" id="groupe_'+i+'_select_id_1" style=""></select></div><div class="form-group"><label for="groupe_'+i+'_select_id_2">Sélectionner le second du groupe A:</label><p class="text-primary col-md-12" id="groupe_'+i+'_select_voted_2" style="display:none">Vous avez voté :</p><select class="form-control" id="groupe_'+i+'_select_id_2" style=""></select></div><button type="submit" class="btn btn-default" id="groupe_'+i+'_btn_id">Submit</button><img id="groupe_'+i+'_gif_submit" src="../images/ajax-loader-mid.gif" style="height:auto; width:auto; display:none;"><p class="text-success" id="groupe_'+i+'_submit_OK" style="height:auto; width:auto; display:none;"> Vote Envoyé !</p><p class="text-danger" id="groupe_'+i+'_submit_KO" style="height:auto; width:auto; display:none;"> Vote NON Envoyé ! rééssayez plus tard !</p><p class="text-warning" id="groupe_'+i+'_submit_KO_doublon" style="height:auto; width:auto; display:none;"> Vote NON Envoyé ! Choisir une équipe différente pour chaque classement !</p></form></div></div></div></div></div>';
-		//on rempli les select
-		for (var z in groupesEuro){
-			if(z <4){
-				document.getElementById('groupe_'+i+'_select_id_1').innerHTML += "<option value="+z+">"+groupeTmp[z]+"</option>";
-	  			document.getElementById('groupe_'+i+'_select_id_2').innerHTML += "<option value="+z+">"+groupeTmp[z]+"</option>";
-			}
-		}
-	}
-};
-//affiche le html
-remplirPoule();
-/************************************************************************** */
 
 
