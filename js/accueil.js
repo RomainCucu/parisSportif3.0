2099383331f5e1ead211f2655e1f845e330ccf8f
RomainCucu <romain.cunault@gmail.com>
2016-06-09 14:37:18 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -145,13 +145,13 @@ var remplirChoix = function(data){
 		remplirSaLigneVoteVainqueur(getParameterByName('pseudo'), data.VOTER1EURO, data.VOTER2EURO, data.VOTER3EURO);
 		if(data.VAINQUEURSEURO2016.VOTER1EURO){
 			afficher('SELECT_VOTER1EURO_VOTED');
-			document.getElementById('SELECT_VOTER1EURO').value = data.VOTER1EURO;
+			document.getElementById('SELECT_VOTER1EURO').value = data.VAINQUEURSEURO2016.VOTER1EURO;
 		}if(data.VAINQUEURSEURO2016.VOTER2EURO){
 			afficher('SELECT_VOTER2EURO_VOTED');
-			document.getElementById('SELECT_VOTER2EURO').value = data.VOTER2EURO;
+			document.getElementById('SELECT_VOTER2EURO').value = data.VAINQUEURSEURO2016.VOTER2EURO;
 		}if(data.VAINQUEURSEURO2016.VOTER3EURO){
 			afficher('SELECT_VOTER3EURO_VOTED');
-			document.getElementById('SELECT_VOTER3EURO').value = data.VOTER3EURO;
+			document.getElementById('SELECT_VOTER3EURO').value = data.VAINQUEURSEURO2016.VOTER3EURO;
 		}
 	}
 	afficher('SELECT_VOTER1EURO');
